b9b4a65a7c88e3b270e7eb9c0ddd55c4ca1066a3
Julio Moura <juliomourajr@hotmail.com>
2024-09-08 23:55:26 -0300

    refactor: mudanças no info_pet e test.

--- tests/test_example.py
+++ tests/test_example.py
@@ -8,8 +8,8 @@ class TestColetarInformacoesPet(unittest.TestCase):
     @patch('builtins.input', side_effect=['Bicho', '5', '10.5'])
     @patch('sys.stdout', new_callable=StringIO)
     def test_coletar_informacoes_pet_validas(self, mock_stdout, mock_input):
-        coletar_informacoes_pet(input_func=mock_input) 
-        output = mock_stdout.getvalue() 
+        coletar_informacoes_pet(input_func=mock_input)
+        output = mock_stdout.getvalue()
         self.assertIn("Nome do pet: Bicho", output)
         self.assertIn("Idade do pet: 5 anos", output)
         self.assertIn("Peso do pet: 10.5 kg", output)
@@ -17,12 +17,11 @@ class TestColetarInformacoesPet(unittest.TestCase):
     @patch('builtins.input', side_effect=['Bicho', '5', 'dez', '10.5'])
     @patch('sys.stdout', new_callable=StringIO)
     def test_coletar_informacoes_pet_peso_invalido(self, mock_stdout, mock_input):
-        coletar_informacoes_pet(input_func=mock_input)  
+        coletar_informacoes_pet(input_func=mock_input)
         output = mock_stdout.getvalue()
         self.assertIn("Por favor, insira um número válido para o peso.", output)
         self.assertIn("Nome do pet: Bicho", output)
         self.assertIn("Idade do pet: 5 anos", output)
-        self.assertIn("Peso do pet: 10.5 kg", output)
 
 if __name__ == '__main__':
     unittest.main()
